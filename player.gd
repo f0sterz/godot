f12c2cbc17bc51db8a20af7a753cd9216df84a07
Roman <f0sterzyt@gmail.com>
2025-06-11 14:35:14 +0500

    Правильна коллизия

--- player.gd
+++ player.gd
@@ -1,31 +1,31 @@
 extends CharacterBody3D
 #@onready var _cam = $CameraPivot/SpringArm3D/Camera3D as Camera3D
 @onready var _cam = $Pivot/Character/CameraPivot/Camera3D as Camera3D
+@onready var _collision = $CollisionShape3D as CollisionShape3D
 # How fast the player moves in meters per second.
-@export var speed = 0
-@export var speedback = 0
+@export var speed:float = 0
+@export var speedback:float = 0
 @export var velo = Vector3()
 #@export var targetcam = target_velocity
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 200
-var angle = 0
-var rota2 = Vector3(0,1,0)
-var direction = Vector3.ZERO
+var angle:float = 0
 var my_basis = Basis.IDENTITY
-var anglespeedl =0
-var anglespeedr =0
+var anglespeedl:float =0
+var anglespeedr:float =0
 ##
 var aaa = 0
 func _physics_process(delta):
 	# We create a local variable to store the input direction.
 	var target_velocity = Vector3()
-	velo = target_velocity
-												# We check for each move input and update the direction accordingly.
+													# We check for each move input and update the direction accordingly.
 	#aaa-=0.001
 	_cam.position = Vector3(0,0,0)
 	
 	#_cam.rotate_y(0.001)
-	if Input.is_action_pressed("move_forward"):
+	if Input.is_action_pressed("move_forward"): #&& speedback==0:
+		speedback+=1
+		if speedback>=0:	
 			while speed < 150:
 				speed+=1
 				break
@@ -36,17 +36,19 @@ func _physics_process(delta):
 			while anglespeedl < 0.05:
 				anglespeedl+=0.003
 				break
-			angle=angle+speed*0.00005 +  anglespeedl
-			$Pivot.basis = basis.rotated(Vector3.UP, angle)
+			angle=angle+speed*0.00005 + anglespeedl
+			
 				
 		if Input.is_action_pressed("move_right"):
 			while anglespeedr < 0.05:
 				anglespeedr+=0.005
 				break
 			angle=angle-anglespeedr
-			$Pivot.basis = basis.rotated(Vector3.UP, angle)
+			
 	
 	if Input.is_action_pressed("move_back"):
+		speed-=0.5
+		if speed<=0:	
 			while speedback > -150:
 				speedback-=1
 				break
@@ -55,41 +57,37 @@ func _physics_process(delta):
 	if speedback < 0:
 		if Input.is_action_pressed("move_left"):
 			angle-=0.05
-			$Pivot.basis = basis.rotated(Vector3.UP, angle)
+			
 				
 		if Input.is_action_pressed("move_right"):
 			angle+=0.05
-			$Pivot.basis = basis.rotated(Vector3.UP, angle)
-					
-	
+			
+
 	
 	if (Input.is_action_pressed("move_forward") == false):
-		while speed >= 0 :
+		while speed > 0 :
 			speed -= 1
 			break
-			
-				
+					
 	if (Input.is_action_pressed("move_back") == false):
-		while speedback <= 0 :
+		while speedback < 0 :
 			speedback += 1
 			break		
 					
-				
-	
 	if (Input.is_action_pressed("move_left") == false):
 		anglespeedl=0	
 	if (Input.is_action_pressed("move_right") == false):
 		anglespeedr=0	
 	
 	#
-				
-	
 	if Input.is_action_pressed("jump"):
-		angle +=5
-		target_velocity.y += 1 
+		#angle +=5
+		target_velocity.y += 5 
 	
 	
-	
+	$Pivot.basis = basis.rotated(Vector3.UP, angle)
+	_collision.basis = basis.rotated(Vector3.UP, angle)
+	#$player.basis = basis.rotated(Vector3.UP, angle)
 	
 	
 	
